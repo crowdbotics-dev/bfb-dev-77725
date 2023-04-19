2b2e2d4768d34632cb929fa95eef19f538b5bbd2
Crowdbotics <engineering@crowdbotics.com>
2023-04-19 22:14:56 +0000

    Initial commit.

---
+++ backend/bfb_dev_77725/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bfb_dev_77725 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bfb_dev_77725.settings')
+
+application = get_wsgi_application()
